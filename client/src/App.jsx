4b07551009beda9077940385d6908bc51597f99e
yogesh2112 <ykphogat21@gmail.com>
2024-07-05 11:35:08 +0530

    File Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
-import client from "./components/apolloClient"; // Import your Apollo Client instance here
-import ProductListPage from "./components/ProductListPage";
-import ProductDetailPage from "./components/ProductDetailPage";
-import ShoppingCart from "./components/ShoppingCart";
-import ShippingAddressForm from "./components/ShippingAddressForm";
-import SigninPage from "./components/signinPage";
-import SignupPage from "./components/signupPage";
-import Header from "./components/header";
-import Footer from "./components/footer";
-import TutorialsPage from "./components/TutorialsPage";
-import FullBlogPost from "./components/FullBlogPost";
-import BlogPage from "./components/BlogPage";
-import AddingProduct from "./components/AddingProduct";
-import AboutUs from "./components/aboutusPage";
-import { LocaleProvider } from "./components/localeContextProvider";
-import OrderConfirmation from "./components/orderConfirmation";
-import OrderHistoryPage from "./components/OrderHistory";
-import HomePage from "./components/HomePage";
-import ProtectedRoute from "./components/ProtectedRoute";
+import client from "./components/HelperComp/apolloClient";
+import { LocaleProvider } from "./components/HelperComp/localeContextProvider";
+import ProtectedRoute from "./components/HelperComp/ProtectedRoute";
+
+import Header from "./components/HeaderComp/header";
+import Footer from "./components/FooterComp/footer";
+import HomePage from "./components/HomePageComp/HomePage";
+import AboutUs from "./components/AboutUsComp/aboutusPage";
+import SigninPage from "./components/SignInComp/signinPage";
+import SignupPage from "./components/SignUpComp/signupPage";
+import ProductListPage from "./components/PLPComp/ProductListPage";
+import ProductDetailPage from "./components/PDPComp/ProductDetailPage";
+import BlogPage from "./components/BlogPageComp/BlogPage";
+import FullBlogPost from "./components/BlogPageComp/FullBlogPost";
+import TutorialsPage from "./components/TutorialsPageComp/TutorialsPage";
+import ShoppingCart from "./components/ShoppingCartComp/ShoppingCart";
+import ShippingAddressForm from "./components/ShoppingCartComp/ShippingAddressForm";
+import AddingProduct from "./components/AddProductComp/AddingProduct";
+import OrderConfirmation from "./components/OrderComfirmationComp/orderConfirmation";
+import OrderHistoryPage from "./components/OrderHistoryComp/OrderHistory";
 
 function App() {
   return (
@@ -33,7 +34,10 @@ function App() {
               <Route path="/cart" element={<ShoppingCart />} />
               <Route path="/" element={<HomePage />} />
               <Route path="/signin" element={<SigninPage />} />
-              <Route path="/delivery-address" element={<ShippingAddressForm />} />
+              <Route
+                path="/delivery-address"
+                element={<ShippingAddressForm />}
+              />
               <Route path="/signup" element={<SignupPage />} />
               <Route path="/header" element={<Header />} />
               <Route path="/footer" element={<Footer />} />
@@ -42,29 +46,29 @@ function App() {
               <Route path="/blogcontent" element={<FullBlogPost />} />
               <Route path="/aboutus" element={<AboutUs />} />
               <Route
-              path="/order-history"
-              element={
-                <ProtectedRoute>
-                  <OrderHistoryPage />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/add-product"
-              element={
-                <ProtectedRoute>
-                  <AddingProduct />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/order-confirm"
-              element={
-                <ProtectedRoute>
-                  <OrderConfirmation />
-                </ProtectedRoute>
-              }
-            />
+                path="/order-history"
+                element={
+                  <ProtectedRoute>
+                    <OrderHistoryPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/add-product"
+                element={
+                  <ProtectedRoute>
+                    <AddingProduct />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/order-confirm"
+                element={
+                  <ProtectedRoute>
+                    <OrderConfirmation />
+                  </ProtectedRoute>
+                }
+              />
             </Routes>
             <Footer />
           </BrowserRouter>
